b5c1f2e0873e3ba3b6f98defad32cc2f9c525529
selcuk baki <selcukbaki@windowslive.com>
2022-03-15 11:58:05 +0300

    Initial Commit

---
+++ Tiktok/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tiktok
+//
+//  Created by Selcuk Baki on 15/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tiktok/TiktokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiktokApp.swift
+//  Tiktok
+//
+//  Created by Selcuk Baki on 15/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TiktokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
